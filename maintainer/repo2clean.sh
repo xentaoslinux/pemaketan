fabafb825ee2d5bcdeae4eaaaab43d7dccc39a77
Dindin Hernawan <dindin_hernawan@yahoo.com>
2019-02-21 23:40:54 +0700

    Update

--- maintainer/repo2clean.sh
+++ maintainer/repo2clean.sh
@@ -13,30 +13,30 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 figlet repo2clean
-repository=$(whoami)/xentaos/pemaketan/repository
+repository=$(whoami)/XentaOS/pemaketan/repository
 cd $repository
 # Bersihkan Index Section Import Xenta OS Repository
-rm -rf  /home/$(whoami)/xentaos/pemaketan/packages/import/*/*.deb
-rm -rf  /home/$(whoami)/xentaos/pemaketan/packages/import/*/*.dsc
-rm -rf  /home/$(whoami)/xentaos/pemaketan/packages/import/*/*.tar.xz
-rm -rf  /home/$(whoami)/xentaos/pemaketan/packages/import/*/*.build
+rm -rf  /home/$(whoami)/XentaOS/pemaketan/packages/import/*/*.deb
+rm -rf  /home/$(whoami)/XentaOS/pemaketan/packages/import/*/*.dsc
+rm -rf  /home/$(whoami)/XentaOS/pemaketan/packages/import/*/*.tar.xz
+rm -rf  /home/$(whoami)/XentaOS/pemaketan/packages/import/*/*.build
 # Bersihkan Index Section Main Xenta OS Repository
-rm -rf  /home/$(whoami)/xentaos/pemaketan/packages/main/*/*.deb
-rm -rf  /home/$(whoami)/xentaos/pemaketan/packages/main/*/*.dsc
-rm -rf  /home/$(whoami)/xentaos/pemaketan/packages/main/*/*.tar.xz
-rm -rf  /home/$(whoami)/xentaos/pemaketan/packages/main/*/*.build
+rm -rf  /home/$(whoami)/XentaOS/pemaketan/packages/main/*/*.deb
+rm -rf  /home/$(whoami)/XentaOS/pemaketan/packages/main/*/*.dsc
+rm -rf  /home/$(whoami)/XentaOS/pemaketan/packages/main/*/*.tar.xz
+rm -rf  /home/$(whoami)/XentaOS/pemaketan/packages/main/*/*.build
 # Bersihkan Index Section Queen Xenta OS Repository
-rm -rf  /home/$(whoami)/xentaos/pemaketan/packages/queen/*/*.deb
-rm -rf  /home/$(whoami)/xentaos/pemaketan/packages/queen/*/*.dsc
-rm -rf  /home/$(whoami)/xentaos/pemaketan/packages/queen/*/*.tar.xz
-rm -rf  /home/$(whoami)/xentaos/pemaketan/packages/queen/*/*.build
+rm -rf  /home/$(whoami)/XentaOS/pemaketan/packages/queen/*/*.deb
+rm -rf  /home/$(whoami)/XentaOS/pemaketan/packages/queen/*/*.dsc
+rm -rf  /home/$(whoami)/XentaOS/pemaketan/packages/queen/*/*.tar.xz
+rm -rf  /home/$(whoami)/XentaOS/pemaketan/packages/queen/*/*.build
 # Bersihkan Index Section Restricted Xenta OS Repository
-rm -rf  /home/$(whoami)/xentaos/pemaketan/packages/restricted/*/*.deb
-rm -rf  /home/$(whoami)/xentaos/pemaketan/packages/restricted/*/*.dsc
-rm -rf  /home/$(whoami)/xentaos/pemaketan/packages/restricted/*/*.tar.xz
-rm -rf  /home/$(whoami)/xentaos/pemaketan/packages/restricted/*/*.build
+rm -rf  /home/$(whoami)/XentaOS/pemaketan/packages/restricted/*/*.deb
+rm -rf  /home/$(whoami)/XentaOS/pemaketan/packages/restricted/*/*.dsc
+rm -rf  /home/$(whoami)/XentaOS/pemaketan/packages/restricted/*/*.tar.xz
+rm -rf  /home/$(whoami)/XentaOS/pemaketan/packages/restricted/*/*.build
 # Bersihkan Index Section Upstream Xenta OS Repository
-rm -rf  /home/$(whoami)/xentaos/pemaketan/packages/upstream/*/*.deb
-rm -rf  /home/$(whoami)/xentaos/pemaketan/packages/upstream/*/*.dsc
-rm -rf  /home/$(whoami)/xentaos/pemaketan/packages/upstream/*/*.tar.xz
-rm -rf  /home/$(whoami)/xentaos/pemaketan/packages/upstream/*/*.build
+rm -rf  /home/$(whoami)/XentaOS/pemaketan/packages/upstream/*/*.deb
+rm -rf  /home/$(whoami)/XentaOS/pemaketan/packages/upstream/*/*.dsc
+rm -rf  /home/$(whoami)/XentaOS/pemaketan/packages/upstream/*/*.tar.xz
+rm -rf  /home/$(whoami)/XentaOS/pemaketan/packages/upstream/*/*.build
--- maintainer/repo2list.sh
+++ maintainer/repo2list.sh
@@ -13,6 +13,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 figlet repo2list
-repository=/home/$(whoami)/xentaos/pemaketan/repository
+repository=/home/$(whoami)/XentaOS/pemaketan/repository
 cd $repository
-reprepro list arok > ../database/PACKAGES.list
+reprepro list batik > ../database/PACKAGES.list
