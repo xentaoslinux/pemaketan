0c281a175ad8a710297277b2f1159240a2cff9aa
Dindin Hernawan <dindin_hernawan@yahoo.com>
2019-03-05 20:57:08 +0700

    Update

--- packages/main/x/xenta-meta.sh
+++ packages/main/x/xenta-meta.sh
@@ -12,7 +12,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-package="xenta-meta-core"
+package="xenta-meta"
 figlet $package
 mkdir $package
 cd $package
