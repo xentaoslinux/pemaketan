4a7a65b60db8ed31094b63b66f37399eadf7a93c
Dindin Hernawan <dindin_hernawan@yahoo.com>
2019-03-08 19:10:03 +0700

    Update

--- packages/upstream/g/gnome-screenshot.sh
+++ packages/upstream/g/gnome-screenshot.sh
@@ -12,7 +12,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-package="ganti-dengan-nama-repo"
+package="gnome-screenshot"
 figlet $package
 mkdir $package
 cd $package
